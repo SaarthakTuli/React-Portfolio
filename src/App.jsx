12b0111bb09acf88d65c557fb81013283b186f35
SaarthakTuli <saarthaktuli@gmail.com>
2022-05-30 23:35:34 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navigator from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
 
 const App = () => {
-    return <div> App </div>;
+    return (
+        <>
+            <Header />
+            <Navigator />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+        </>
+    )
 };
 
 export default App;
