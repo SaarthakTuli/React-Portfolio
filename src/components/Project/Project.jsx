e87fdde6f7df8d77d90f8dcf7a891174c3b791d8
SaarthakTuli <saarthaktuli@gmail.com>
2022-06-04 03:22:26 +0530

    Project 1

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,40 +1,92 @@
 import React from 'react'
 import './project.css'
-import ME from '../../assets/Saarthak.png'
 import img1 from '../../assets/proj1-1.jpg'
 import img2 from '../../assets/proj1-2.jpg'
 import img3 from '../../assets/proj1-3.jpg'
 import img4 from '../../assets/proj1-4.jpg'
 
+// Import Swiper core
+import { Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+
+const data = [
+    {
+        avatar: img1,
+        name: "Light Mode",
+        review: "This is the light mode login page with the icons that rotate and circle constantly.. The login button helps you login int the app using Google Sign in."
+    },
+
+    {
+        avatar: img2,
+        name: "Light Mode",
+        review: "This is the light mode login page.. in thi spage you see your name displayed and the 5 most recent transactions made.. Ther is also a plus tool to add more transactions to the database.."
+    },
+
+    {
+        avatar: img3,
+        name: "Dark Mode",
+        review: "This is the dark mode login page with the icons that rotate and circle constantly.. The login button helps you login int the app using Google Sign in."
+    },
+
+    {
+        avatar: img4,
+        name: "Dark Mode",
+        review: "This is the dark mode login page.. in thi spage you see your name displayed and the 5 most recent transactions made.. Ther is also a plus tool to add more transactions to the database.."
+    }
+]
+
+
+
 const Project = () => {
   return (
     <section id="project">
         <h5>Project 1</h5>
-        <h2>Expenditor</h2>
+        <h2>Expenditure Tracking App</h2>
 
         <div className="container project__container">
-            
-            <div className="project__me">
-                  <div className="project__me-image">
-                      <h2>Expenditor</h2>
+            <div className="project__detail">
+                <h2>Expenditor</h2>
                     <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi cupiditate iusto, dicta aut amet doloribus eligendi est incidunt minima veritatis distinctio culpa, voluptas reiciendis ab laboriosam fugiat, fugit quaerat accusamus.
+                        This is the light mode login page.. in thi spage you see your name displayed and the 5 most recent transactions made.. Ther is also a plus tool to add more transactions to the database..
                     </p>
-                </div>
             </div>
 
-            <div className="project__content">
-                <div className="project__cards">
-                    <div className="project__item">
-                        <img src={img1} alt="me" className='project__img' />
-                        <img src={img2} alt="me" className='project__img' />
-                    </div>
-                    <div className="project__item">
-                        <img src={img3} alt="me" className='project__img' />
-                        <img src={img4} alt="me" className='project__img' />
-                    </div>
-                </div>
-            </div>
+            <Swiper className="project__content"
+                modules={[Pagination]}
+                spaceBetween={10}
+                slidesPerView={2}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ clickable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={(swiper) => console.log('slides change')}>
+                
+                { 
+                    data.map(({ avatar, name, review }, index) => { 
+                        return (
+                            <SwiperSlide key={index} className='projects'>
+                                
+                                <div className='project__slide'>
+
+                                    <div className='proj__avatar'>
+                                        <img src={avatar} alt="Image" />
+                                        <p>{ review}</p>
+                                    </div>
+
+                                    
+                                    
+                                </div> 
+                            </SwiperSlide>
+                        )
+                    })
+                }
+
+        </Swiper>
 
         </div>
     </section>
